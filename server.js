75076ea935cfd868657e331b1b7a0d5a1f81e271
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-09-19 21:01:55 -0400

    alright - now actually have the intial setup and libraries installed (express, mongoose, path, morgan)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const morgan = require("morgan");
-var mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 // Define middleware here
 app.use(morgan("dev"));
